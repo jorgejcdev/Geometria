970a5f8436dbe2e8f41593b7b5e491ab02f78dd6
jorgejcdev <110706936+jorgejcdev@users.noreply.github.com>
2022-11-03 18:14:10 -0300

    Add files via upload

---
+++ geometria.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+    int i,j;
+
+    for(i = 1; i<= 20 ; i++){
+       printf(" @");
+     }
+
+       printf("\n");
+ 
+    for(i = 2;i <6;i++) {
+       printf(" @");
+   
+    for(j = 2;j < 20;j++)   {
+       printf("  ");           
+   }
+       printf(" @  \n");
+
+ }
+
+     for(i = 1; i<= 20 ; i++){       
+       printf(" @");
+
+ }
+}
